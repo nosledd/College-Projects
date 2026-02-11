ebabad1150587fddcc4e5f6059432a4ce71fbbe6
nosledd <delsonmonteiro625@gmail.com>
2026-02-11 21:42:23 +0530

    Main

--- Python/File Automation/main.py
+++ Python/File Automation/main.py
@@ -1,18 +1,22 @@
+import os
+import shutil
 from tkinter import filedialog
-from sorter import sort_file
 
-def upload_and_sort():
-    
-    # Open file picker
-    file_path = filedialog.askopenfilename(
-        title="Select Notes/PDF",
-        filetypes=[("PDF Files", "*.pdf"), ("All Files", "*.*")]
-    )
-    
-    if file_path:
-        subject = input("Enter Subject Name: ")
-        new_path = sort_file(file_path, subject)
-        print("Sorted Path:", new_path)
+def file_access():
+    file_path=filedialog.askopenfilename()
+    print(file_path)
 
-# Run function
-upload_and_sort()
+    file_name=os.path.basename(file_path)
+    print(file_name)
+
+    extension=os.path.splitext(file_path)[1]
+    print(extension)
+
+    destination= "E:/Git/College-Projects/Python/File Automation/uploads/" + file_name
+    shutil.copy(file_path,destination)
+
+
+file_access()
+    
+
+
