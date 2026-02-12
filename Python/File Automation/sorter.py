58bb50fd7214823f4738b947a9c9ee0b9ef8f195
nosledd <delsonmonteiro625@gmail.com>
2026-02-12 23:06:53 +0530

    sorter

--- Python/File Automation/sorter.py
+++ Python/File Automation/sorter.py
@@ -1,21 +1,8 @@
-import os
-import shutil
+def sortet(file_name):
+    subjects={
+        "Maths":["algebra", "addition"],
+        "Yt":["Who needs roads lol"]
+    }
 
-def sort_file(file_path, subject):
-    
-    # Create subject folder if not exists
-    destination_folder = f"sorted_notes/{subject}"
-    os.makedirs(destination_folder, exist_ok=True)
-    
-    # Get file name
-    file_name = os.path.basename(file_path)
-    
-    # Destination path
-    destination_path = os.path.join(destination_folder, file_name)
-    
-    # Move file
-    shutil.move(file_path, destination_path)
-    
-    print(f"File moved to {destination_folder}")
-    
-    return destination_path
+    file_name=file_name.lower()
+    print(file_name)
