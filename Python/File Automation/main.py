0eb0fff0d3e8fcbaef5629bee1099fd2102920e0
nosledd <delsonmonteiro625@gmail.com>
2026-02-13 21:54:00 +0530

    main

--- Python/File Automation/main.py
+++ Python/File Automation/main.py
@@ -10,7 +10,7 @@ def file_access():
     root = tk.Tk()
     root.withdraw() #i am hidding the ghost window
 
-    selected_path = filedialog.askdirectory(title="Create a Folder")
+    folders = filedialog.askdirectory(title="Create a Folder")
 
     file_path=filedialog.askopenfilename()
     print(file_path)
@@ -18,12 +18,12 @@ def file_access():
     file_name=os.path.basename(file_path)
     print(file_name)
 
-    extension=os.path.splitext(file_path)[1]
+    extension=os.path.splitext(file_name)[0]
     print(extension)
 
     destination= "E:/Git/College-Projects/Python/File Automation/uploads/" + file_name
     shutil.copy(file_path,destination)
-    sorter.sortet(file_name)
+    sorter.sortet(file_name,folders)
 
 
 file_access()
