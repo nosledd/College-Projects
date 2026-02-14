d190c20396ea00af841a11e59c6ea3f64857d09e
nosledd <delsonmonteiro625@gmail.com>
2026-02-14 22:11:35 +0530

    main

--- Python/File Automation/main.py
+++ Python/File Automation/main.py
@@ -25,7 +25,30 @@ file_access()
 
 
 
+import os
+import shutil
+from tkinter import filedialog
+import sorter
+import tkinter as tk
 
+
+def file_access():
+    root = tk.Tk()
+    root.withdraw() #i am hidding the ghost window
+
+    # folders = filedialog.askdirectory(title="Create a Folder")
+
+    file_path=filedialog.askopenfilename()
+    print(file_path)
+
+    file_name=os.path.basename(file_path)
+    print(file_name)
+
+    extension=os.path.splitext(file_name)[0]
+    print(extension)
+
+
+file_access()
 
 
 
