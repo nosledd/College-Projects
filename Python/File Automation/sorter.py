b9b26246e10d817a9c9e44a6ada1a07c4e863985
nosledd <delsonmonteiro625@gmail.com>
2026-02-15 19:28:38 +0530

    sorter

--- Python/File Automation/sorter.py
+++ Python/File Automation/sorter.py
@@ -16,15 +16,5 @@ def sortet(file_name):
              sorter.sortet(file_name,)
              break
         else:
-           continue 
-
-
-
-
-
-           for i in subjects:
-        print(i)
-        if (file_name == i):
-             destination= "E:/Git/College-Projects/Python/File Automation/uploads/" + file_name
-             
+           continue             
         
