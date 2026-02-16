064df2628434841f5fd83b644193fa4e90da4b13
nosledd <delsonmonteiro625@gmail.com>
2026-02-16 19:55:45 +0530

    sorter

--- Python/File Automation/sorter.py
+++ Python/File Automation/sorter.py
@@ -1,22 +1,30 @@
 import shutil
 
-def sortet(file_path,extension):
+def sortet(file_path,file_name):
     
-    extension=extension.lower()
-    print("name",extension)
+    file_name=file_name.lower()
+    print("name",file_name)
 
     subjects={
-        "flutter":["flutter.pdf", "addition"],
+        "flutter":["flutter", "addition"],
         "Yt":["Who needs roads lol"]
     }
 
-    for i in subjects:
-        print("Loop",i)
-        if (extension == i):
+    for subject in subjects:
+        print(subject)
+        if subject in file_name:
+            print("Found Thrpugh file name")
+        else:
+             for keyword in subjects[subject]:
+                 if keyword in file_name:
+                     print("Found through keyword")
+        
+        
+    '''if (file_name == i):
              destination= "E:/Git/College-Projects/Python/File Automation/uploads/sorted_notes/Python/"
              print(file_path)
              shutil.copy(file_path,destination)
              break
         else:
-           print("Error")         
+           print("Error")  '''       
         
