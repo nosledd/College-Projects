856cde62a773ebafe4a5e30efb8613f38a6aaaed
nosledd <delsonmonteiro625@gmail.com>
2026-03-08 21:28:20 +0530

    sorter

--- Python/File Automation/sorter.py
+++ Python/File Automation/sorter.py
@@ -6,7 +6,7 @@ import database
 def sortet(file_path,file_name):
     
     file_name=file_name.lower()
-    print("name",file_name)
+    print("lower case name",file_name)
 
     subjects={
         "flutter":["flutter", "addition"],
@@ -23,16 +23,12 @@ def sortet(file_path,file_name):
             database.insert_record(file_name, subject, destination)
             break
         else:
-            for keyword in subjects[subject]:
-                if keyword in file_name:
-                    destination= "E:/Git/College-Projects/Python/File Automation/uploads/sorted_notes/"+subject+"/"
-                    if not os.path.exists(destination):
-                         os.makedirs(destination)
+            destination= "E:/Git/College-Projects/Python/File Automation/uploads/sorted_notes/others/"
+            if not os.path.exists(destination):
+                    os.makedirs(destination)
             
-                    shutil.copy(file_path,destination)
-                    database.insert_record(file_name, subject, destination)
-                    break
+            shutil.copy(file_path,destination)
+            database.insert_record(file_name, subject, destination)
+            break
         
-# theres still lot of logic work to be done
-     
-        
+# pending       
